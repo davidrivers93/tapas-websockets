d32ebaef5499d9e75ff6041adfa39cce5847ffb7
David <davidriosbenet@gmail.com>
2021-05-10 15:18:54 +0200

    feat: Create chat sockets

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,14 @@ import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 
 import { join } from 'path';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ChatModule } from './chat/chat.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client'),
     }),
+    ChatModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
---
+++ src/chat/chat.gateway.ts
@@ -0,0 +1,38 @@
+import { Logger } from '@nestjs/common';
+import {
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway()
+export class ChatGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  private logger: Logger = new Logger('ChatGateway');
+
+  @WebSocketServer()
+  server: Server;
+
+  @SubscribeMessage('message')
+  handleChatMessage(@MessageBody() message: string): void {
+    this.server.emit('message', message);
+  }
+
+  afterInit(server: Server) {
+    this.logger.log('Init');
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client connected: ${client.id}`);
+  }
+}
